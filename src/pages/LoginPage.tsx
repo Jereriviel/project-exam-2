c239b39d8987377742fb2f39f56d60b84cb0010d
Carina M. Pedersen <carina.m.pedersen@gmail.com>
2026-04-30 13:07:15 +0200

    Fixes small helmet error on login/register pages

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,27 +3,29 @@ import LoginForm from "../components/features/auth/LoginForm";
 import { Link } from "react-router-dom";
 
 function LoginPage() {
-  <Helmet>
-    <title>Holidaze | Login</title>
-    <meta
-      name="description"
-      content="Log into your personal account on Holidaze"
-    />
-  </Helmet>;
-
   return (
-    <div className="w-full max-w-100 space-y-8 px-4">
-      <LoginForm />
-      <div className="flex flex-col gap-2 sm:flex-row">
-        <p>Don't have an account?</p>
-        <Link
-          className="hover:text-primary font-semibold transition duration-500 ease-in-out"
-          to={"/register"}
-        >
-          Register here
-        </Link>
+    <>
+      <Helmet>
+        <title>Holidaze | Login</title>
+        <meta
+          name="description"
+          content="Log into your personal account on Holidaze"
+        />
+      </Helmet>
+
+      <div className="w-full max-w-100 space-y-8 px-4">
+        <LoginForm />
+        <div className="flex flex-col gap-2 sm:flex-row">
+          <p>Don't have an account?</p>
+          <Link
+            className="hover:text-primary font-semibold transition duration-500 ease-in-out"
+            to={"/register"}
+          >
+            Register here
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -3,27 +3,29 @@ import RegisterForm from "../components/features/auth/RegisterForm";
 import { Link } from "react-router-dom";
 
 function RegisterPage() {
-  <Helmet>
-    <title>Holidaze | Register</title>
-    <meta
-      name="description"
-      content="Register your personal account on Holidaze"
-    />
-  </Helmet>;
-
   return (
-    <div className="w-full max-w-100 space-y-8 px-4">
-      <RegisterForm />
-      <div className="flex flex-col gap-2 sm:flex-row">
-        <p>Already have an account?</p>
-        <Link
-          className="hover:text-primary font-semibold transition duration-500 ease-in-out"
-          to={"/login"}
-        >
-          Log in here
-        </Link>
+    <>
+      <Helmet>
+        <title>Holidaze | Register</title>
+        <meta
+          name="description"
+          content="Register your personal account on Holidaze"
+        />
+      </Helmet>
+
+      <div className="w-full max-w-100 space-y-8 px-4">
+        <RegisterForm />
+        <div className="flex flex-col gap-2 sm:flex-row">
+          <p>Already have an account?</p>
+          <Link
+            className="hover:text-primary font-semibold transition duration-500 ease-in-out"
+            to={"/login"}
+          >
+            Log in here
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
