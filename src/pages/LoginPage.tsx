0f21bf1a4fa139105277fff32f89eb6df007d7e8
Carina M. Pedersen <carina.m.pedersen@gmail.com>
2026-04-30 12:39:35 +0200

    Issue #7 Adds title and description after testing

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,20 @@
+import { useEffect } from "react";
 import LoginForm from "../components/features/auth/LoginForm";
 import { Link } from "react-router-dom";
 
 function LoginPage() {
+  useEffect(() => {
+    document.title = "Holidaze| Login";
+
+    const meta = document.querySelector("meta[name='description']");
+    if (meta) {
+      meta.setAttribute(
+        "content",
+        "Log into your personal account on Holidaze",
+      );
+    }
+  }, []);
+
   return (
     <div className="w-full max-w-100 space-y-8 px-4">
       <LoginForm />
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
 import RegisterForm from "../components/features/auth/RegisterForm";
 import { Link } from "react-router-dom";
 
 function RegisterPage() {
+  useEffect(() => {
+    document.title = "Holidaze| Register";
+
+    const meta = document.querySelector("meta[name='description']");
+    if (meta) {
+      meta.setAttribute("content", "Create a new personal account on Holidaze");
+    }
+  }, []);
+
   return (
     <div className="w-full max-w-100 space-y-8 px-4">
       <RegisterForm />
