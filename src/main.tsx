a9c13e74ea13c9e9588cd79c0f25471935f6ecfd
Carina M. Pedersen <carina.m.pedersen@gmail.com>
2026-04-30 15:00:12 +0200

    Adds QueryClientProvider to main.tsx and VenuesResponse interface to venue.ts

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,19 @@ import App from "./App.tsx";
 import AuthProvider from "./context/AuthProvider.tsx";
 import { ToastContainer } from "react-toastify";
 import { HelmetProvider } from "react-helmet-async";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ToastContainer />
     <HelmetProvider>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </QueryClientProvider>
     </HelmetProvider>
   </StrictMode>,
 );
