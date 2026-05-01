b6ec1fe45f5d33f95ce7f272000995834cf288b6
Carina M. Pedersen <carina.m.pedersen@gmail.com>
2026-05-01 12:11:36 +0200

    Creates and implements fallbackImg to venue card

--- src/components/features/venues/VenueCard.tsx
+++ src/components/features/venues/VenueCard.tsx
@@ -1,20 +1,27 @@
 import type { Venue } from "../../../types/venue";
 import { Link } from "react-router-dom";
 import Rating from "./Rating";
+import fallbackImg from "../../../../public/fallback-img.jpg";
 
 type VenueCardProps = {
   venue: Venue;
 };
 
 const VenueCard = ({ venue }: VenueCardProps) => {
+  const mainImage = venue.media[0];
+  const imageSrc = mainImage?.url ? mainImage.url : fallbackImg;
+
   return (
     <>
       <article className="grid-cols-[minmax(0, 1fr)] grid grid-rows-[max-content] rounded-xl transition duration-300 ease-in-out sm:hover:scale-102">
         <Link className="min-w-0" to={`/venue/${venue.id}`}>
           <img
-            src={venue.media[0]?.url}
-            alt={venue.media[0]?.alt}
+            src={imageSrc}
+            alt={venue.media[0]?.alt || "Venue image"}
             className="aspect-4/3 w-full rounded-xl object-cover"
+            onError={(e) => {
+              (e.currentTarget as HTMLImageElement).src = fallbackImg;
+            }}
           />
           <div className="grid-cols-auto grid py-4">
             <div className="mt-auto flex min-w-0 flex-col gap-2">
