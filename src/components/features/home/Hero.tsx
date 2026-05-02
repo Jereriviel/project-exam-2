c5e8bb767ca3672c1c01aeab306a772568ff1204
Carina M. Pedersen <carina.m.pedersen@gmail.com>
2026-05-02 10:34:18 +0200

    Changes the Hero component container to a section

--- src/components/features/home/Hero.tsx
+++ src/components/features/home/Hero.tsx
@@ -1,6 +1,6 @@
 const Hero = () => {
   return (
-    <div className="bg-secondary-light flex w-full flex-col gap-4 px-4 py-8 sm:items-center sm:gap-8 sm:px-8 sm:text-center">
+    <section className="bg-secondary-light flex w-full flex-col gap-4 px-4 py-8 sm:items-center sm:gap-8 sm:px-8 sm:text-center">
       <h1 className="font-display text-4xl font-bold sm:text-6xl">
         <span className="text-primary"> Where </span>do you wish to{" "}
         <span className="text-primary">stay</span>?
@@ -8,7 +8,7 @@ const Hero = () => {
       <p className="text-lg font-light md:text-2xl">
         Find your new favourite holiday accommodation here
       </p>
-    </div>
+    </section>
   );
 };
 
