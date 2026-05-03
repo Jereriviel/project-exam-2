4cb5376197ffbc9ec2d0224820a8cacd60cc3a45
Carina M. Pedersen <carina.m.pedersen@gmail.com>
2026-05-03 10:39:30 +0200

    Adds SearchBar to Header

--- src/components/features/home/Hero.tsx
+++ src/components/features/home/Hero.tsx
@@ -1,6 +1,6 @@
 const Hero = () => {
   return (
-    <section className="bg-secondary-light flex w-full flex-col gap-4 px-4 py-8 sm:items-center sm:gap-8 sm:px-8 sm:text-center">
+    <section className="bg-secondary-light flex w-full flex-col gap-4 px-4 pt-4 pb-8 sm:items-center sm:gap-8 sm:px-8 sm:pt-8 sm:pb-12 sm:text-center">
       <h1 className="font-display text-4xl font-bold sm:text-6xl">
         <span className="text-primary"> Where </span>do you wish to{" "}
         <span className="text-primary">stay</span>?
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 import logoPrimary from "../../../public/holidaze_logo_primary.svg";
+import SearchBar from "../features/search/SearchBar";
 
 const Header = () => {
   return (
     <>
-      <header className="bg-secondary-light sticky top-0 z-50 mt-0 w-full p-4 sm:px-8">
-        <div className="mx-auto flex max-w-7xl items-center justify-between">
+      <header className="bg-secondary-light sticky top-0 z-50 mt-0 w-full space-y-4 p-4 sm:px-8">
+        <div className="mx-auto flex max-w-7xl items-center justify-between sm:gap-12">
           <Link to={"/"} aria-label="Home">
             <img
               src={logoPrimary}
@@ -13,6 +14,10 @@ const Header = () => {
               className="h-8 w-25 object-cover sm:h-12 sm:w-40"
             />
           </Link>
+          <div className="hidden w-full sm:flex">
+            <SearchBar />
+          </div>
+
           <Link to={"/login"} aria-label="Login" className="hidden sm:block">
             <div className="btn-primary">
               <p>Login</p>
@@ -23,6 +28,9 @@ const Header = () => {
               <span className="iconify-[material-symbols--person-outline]"></span>
             </div>
           </Link>
+        </div>
+        <div className="flex w-full sm:hidden">
+          <SearchBar />
         </div>
       </header>
     </>
