42e2b4e114e15fa77c692f38753974d03f6badfd
Carina M. Pedersen <carina.m.pedersen@gmail.com>
2026-05-04 13:20:01 +0200

    Issue #43 Creates Pagination UI component

---
+++ src/components/ui/Pagination.tsx
@@ -0,0 +1,49 @@
+import Pagination from "@mui/material/Pagination";
+import { Stack } from "@mui/material";
+
+type PaginationProps = {
+  currentPage: number;
+  pageCount: number;
+  onPageChange: (page: number) => void;
+};
+
+const VenuePagination = ({
+  currentPage,
+  pageCount,
+  onPageChange,
+}: PaginationProps) => {
+  return (
+    <Stack className="my-10 items-center justify-center">
+      <Pagination
+        count={pageCount}
+        page={currentPage}
+        onChange={(_, page) => onPageChange(page)}
+        sx={{
+          "& .MuiPaginationItem-root:not(.MuiPaginationItem-ellipsis)": {
+            backgroundColor: "#ffffff",
+            color: "#000000",
+            fontWeight: "600",
+            "&:hover": {
+              backgroundColor: "#FFC0AA",
+            },
+            "&.Mui-selected": {
+              backgroundColor: "#FFC0AA",
+              "&:hover": {
+                backgroundColor: "#ffb09a",
+              },
+            },
+          },
+
+          "& .MuiPaginationItem-ellipsis": {
+            fontWeight: "600",
+          },
+        }}
+        shape="circular"
+        size="large"
+        siblingCount={0}
+      />
+    </Stack>
+  );
+};
+
+export default VenuePagination;
