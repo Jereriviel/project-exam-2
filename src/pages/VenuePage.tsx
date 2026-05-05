5b5a13cdd30cbd7a330dd4580c5ec6d89fa61dcc
Carina M. Pedersen <carina.m.pedersen@gmail.com>
2026-05-05 14:15:36 +0200

    Issue #49, adds Helmet title and descripion

--- src/pages/VenuePage.tsx
+++ src/pages/VenuePage.tsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
+import { Helmet } from "react-helmet-async";
 import { getVenueById } from "../api/venues";
 import Container from "../layouts/Container";
 import Breadcrumb from "../components/ui/BreadCrumb";
@@ -21,15 +22,20 @@ function VenuePage() {
     return <p>Couldn't find the venue you're looking for.</p>;
 
   return (
-    <Container>
-      <div className="py-4">
-        <Breadcrumb venue={venue} />
-      </div>
-
-      <section className="w-full py-4">
-        <h1>{venue.name}</h1>
-      </section>
-    </Container>
+    <>
+      <Helmet>
+        <title>Holidaze | {venue.name}</title>
+        <meta name="description" content={venue.description.slice(0, 160)} />
+      </Helmet>
+      <Container>
+        <div className="py-4">
+          <Breadcrumb venue={venue} />
+        </div>
+        <section className="w-full py-4">
+          <h1>{venue.name}</h1>
+        </section>
+      </Container>
+    </>
   );
 }
 
