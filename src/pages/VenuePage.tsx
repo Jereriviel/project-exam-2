b5ce7bc8ab81c41f689845e61f668460cc075ba1
Carina M. Pedersen <carina.m.pedersen@gmail.com>
2026-05-07 10:49:44 +0200

    Issue #49, implements loading skeleton and error modal for VenuePage.tsx

--- src/pages/VenuePage.tsx
+++ src/pages/VenuePage.tsx
@@ -1,113 +1,96 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
 import { getVenueById } from "../api/venues";
 import Container from "../layouts/Container";
 import Breadcrumb from "../components/ui/BreadCrumb";
-import AmenityTagList from "../components/features/venues/AmenityTagList";
 import type { Venue } from "../types/venue";
-import fallbackImg from "../../public/fallback-img.jpg";
-import avatarFallbackImg from "../../public/avatar-fallback-img.jpg";
-import Rating from "../components/features/venues/Rating";
+import VenueDetails from "../components/features/venues/VenueDetails";
+import VenuePageSkeleton from "../components/features/venues/VenuePageLoadingSkeleton";
+import ErrorModal from "../components/ui/ErrorModal";
+import { ApiError } from "../error/ApiError";
 
 function VenuePage() {
   const { id } = useParams<{ id: string }>();
+  const navigate = useNavigate();
+  const [isModalClosed, setIsModalClosed] = useState(false);
+
   const {
     data: venue,
     isLoading,
     isError,
+    error,
   } = useQuery<Venue>({
     queryKey: ["venue", id],
     queryFn: () => getVenueById(id!),
   });
 
-  if (isLoading) return <p>Loading...</p>;
+  useEffect(() => {
+    if (isError && error instanceof ApiError && error.statusCode === 404) {
+      navigate("/404");
+    }
+  }, [isError, error, navigate]);
 
-  if (isError || !venue)
-    return <p>Couldn't find the venue you're looking for.</p>;
+  const apiError =
+    error instanceof ApiError
+      ? error
+      : new ApiError(500, "Server Error", "An unexpected error occurred");
 
-  const mainImage = venue.media[0];
-  const imageSrc = mainImage?.url ? mainImage.url : fallbackImg;
-  const profileImage = venue.owner?.avatar;
-  const profileImageSrc = profileImage?.url
-    ? profileImage.url
-    : avatarFallbackImg;
+  const showModal = isError && apiError.statusCode !== 404 && !isModalClosed;
+
+  if (isLoading) {
+    return (
+      <Container>
+        <div className="pt-4">
+          <div className="fade-in">
+            <VenuePageSkeleton />
+          </div>
+        </div>
+      </Container>
+    );
+  }
+
+  if (!venue && !isError) {
+    return (
+      <Container>
+        <p className="py-4 font-semibold">Venue not found.</p>
+      </Container>
+    );
+  }
 
   return (
     <>
-      <Helmet>
-        <title>Holidaze | {venue.name}</title>
-        <meta name="description" content={venue.description.slice(0, 160)} />
-      </Helmet>
-      <Container>
-        <div className="pt-4">
-          <Breadcrumb venue={venue} />
-        </div>
-        <section className="flex w-full flex-col gap-4 py-4 md:gap-6">
-          <img
-            className="aspect-2/1 w-full rounded-xl object-cover"
-            src={imageSrc}
-            alt={venue.media[0]?.alt}
-            onError={(e) => {
-              (e.currentTarget as HTMLImageElement).src = fallbackImg;
-            }}
-          />
-          <div className="flex flex-col gap-3">
-            <h1 className="text-2xl md:text-3xl">{venue.name}</h1>
-            <div className="flex gap-2 text-sm sm:items-center md:text-base">
-              <span className="iconify-[material-symbols--location-on-outline]"></span>
-              <div className="flex flex-wrap gap-1">
-                <p>{`${venue.location.address || "Address: N/A"}, ${venue.location.zip || "Zip Code: N/A"},`}</p>
-                <p>{`${venue.location.city || "City: N/A"}, ${venue.location.country || "Country: N/A"}`}</p>
-              </div>
-            </div>
-            <div className="flex items-center gap-2">
-              <div className="flex items-center">
-                <Rating rating={venue.rating} />
-              </div>
-              <p
-                className={`text-sm ${!venue.rating ? "text-gray-medium" : "font-semibold"}`}
-              >
-                {venue.rating || "Not rated yet"}
-              </p>
-            </div>
-            <div className="flex gap-2">
-              <p className="text-xl font-semibold md:text-2xl">
-                {venue.price} NOK
-              </p>
-              <p className="text-sm md:text-lg">per night</p>
-            </div>
-          </div>
-          <div className="flex flex-col gap-4">
-            <h2 className="text-xl md:text-2xl">Description</h2>
-            <p className="md:text-lg">{venue.description}</p>
-          </div>
-          <div className="flex flex-col gap-4">
-            <h2 className="text-xl md:text-2xl">Includes</h2>
-            <div className="flex flex-col gap-4">
-              <AmenityTagList meta={venue.meta} />
-              <div className="tag">
-                <span className="iconify-[material-symbols--king-bed-outline]"></span>
-                {`Up to ${venue.maxGuests} guests`}
-              </div>
-            </div>
-          </div>
-          <div className="flex items-center gap-2 md:text-lg">
-            <img
-              className="size-12 rounded-full object-cover"
-              src={profileImageSrc}
-              alt={venue.owner?.avatar.alt}
-              onError={(e) => {
-                (e.currentTarget as HTMLImageElement).src = avatarFallbackImg;
-              }}
+      {venue && (
+        <>
+          <Helmet>
+            <title>Holidaze | {venue.name}</title>
+            <meta
+              name="description"
+              content={venue.description.slice(0, 160)}
             />
-            <div className="flex flex-col">
-              <p className="font-semibold">Venue owner</p>
-              <p>{`${venue.owner?.name || "Unknown"}`}</p>
+          </Helmet>
+
+          <Container>
+            <div className="pt-4">
+              <Breadcrumb venue={venue} />
+
+              <div className="fade-in">
+                <VenueDetails venue={venue} />
+              </div>
             </div>
-          </div>
-        </section>
-      </Container>
+          </Container>
+        </>
+      )}
+      <ErrorModal
+        isOpen={showModal}
+        onClose={() => {
+          setIsModalClosed(true);
+          navigate("/");
+        }}
+        error={showModal ? apiError : null}
+      />
     </>
   );
 }
