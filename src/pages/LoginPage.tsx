b5fa7283a4edc6863d7e970f2cab21c39066ee8c
Carina M. Pedersen <carina.m.pedersen@gmail.com>
2026-05-12 15:47:10 +0200

    Updates Back navigation for register and login pages.

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,8 +1,18 @@
 import { Helmet } from "react-helmet-async";
 import LoginForm from "../components/features/auth/LoginForm";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function LoginPage() {
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    if (window.history.length > 1) {
+      navigate(-1);
+    } else {
+      navigate("/");
+    }
+  };
+
   return (
     <>
       <Helmet>
@@ -15,13 +25,13 @@ function LoginPage() {
 
       <div className="flex w-full flex-1 flex-col items-center justify-center px-4 py-8">
         <div className="flex w-full max-w-100 flex-col gap-8">
-          <Link
+          <button
             className="hover:text-primary flex w-fit items-center gap-2 pr-1 font-semibold transition duration-250 ease-in-out"
-            to="/"
+            onClick={handleBack}
           >
             <span className="iconify-[material-symbols--arrow-back]"></span>
             <p>Back</p>
-          </Link>
+          </button>
           <LoginForm />
           <div className="flex flex-col gap-2 sm:flex-row">
             <p>Don't have an account?</p>
