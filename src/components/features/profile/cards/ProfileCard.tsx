904e4162cc729a7d9046b8af846cfcb98cf0a019
Carina M. Pedersen <carina.m.pedersen@gmail.com>
2026-05-20 13:42:20 +0200

    Issue #52, creates profile card for bookings and venues

---
+++ src/components/features/profile/cards/ProfileCard.tsx
@@ -0,0 +1,40 @@
+import type { ReactNode } from "react";
+import { Link } from "react-router-dom";
+import fallbackImg from "../../../../../public/fallback-img.jpg";
+
+interface ProfileCardProps {
+  title: string;
+  imageUrl?: string;
+  imageAlt?: string;
+  link: string;
+  children: ReactNode;
+}
+
+const ProfileCard = ({
+  title,
+  imageUrl,
+  imageAlt,
+  link,
+  children,
+}: ProfileCardProps) => {
+  return (
+    <article className="grid grid-cols-1 overflow-hidden rounded-xl xl:grid-cols-2">
+      <Link to={link}>
+        <img
+          src={imageUrl || fallbackImg}
+          alt={imageAlt || title}
+          className="aspect-5/3 w-full overflow-hidden rounded-t-xl rounded-b-none object-cover lg:aspect-4/3 xl:rounded-s-xl xl:rounded-e-none"
+        />
+      </Link>
+      <div className="bg-secondary-light flex flex-col gap-4 rounded-t-none rounded-b-xl p-4 xl:rounded-s-none xl:rounded-e-xl">
+        <h3 className="truncate lg:text-xl">{title}</h3>
+        {children}
+        <button type="button" className="btn-primary-sm">
+          Make Changes
+        </button>
+      </div>
+    </article>
+  );
+};
+
+export default ProfileCard;
